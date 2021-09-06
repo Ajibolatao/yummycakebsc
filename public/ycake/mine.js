1e814954ffbfa8d3b4913b9a68c3541b0a7a2fe3
unknown <taolawbal4@gmail.com>
2021-09-06 10:48:08 +0100

    Changes added

--- public/ycake/mine.js
+++ public/ycake/mine.js
@@ -2,8 +2,12 @@ const addressEl = document.querySelector(".address");
 const copyEl = document.querySelector(".bi-clipboard");
 
 copyEl.addEventListener("click", () => {
+  /* Select the text field */
+  addressEl.select();
+  addressEl.setSelectionRange(0, 99999); /* For mobile devices */
+
   /* Copy the text inside the text field */
-  navigator.clipboard.writeText(addressEl.textContent);
+  navigator.clipboard.writeText(addressEl.value);
 
   /* Alert the copied text */
   alert("Copied text to clipboard");
